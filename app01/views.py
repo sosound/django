f118426647ae25befd32fe605a58cbecc25432ed
sosound <clearputaoshu@163.com>
2023-08-20 00:24:26 +0800

    s

--- app01/views.py
+++ app01/views.py
@@ -5,3 +5,11 @@ from django.shortcuts import render, HttpResponse
 
 def index(request):
     return HttpResponse('Hello World!')
+
+
+def user_list(request):
+    return HttpResponse('user list')
+
+
+def user_add(request):
+    return HttpResponse('user add')
--- django4/urls.py
+++ django4/urls.py
@@ -20,5 +20,7 @@ from app01 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', views.index)
+    path('index/', views.index),
+    path('user/list/', views.user_list),
+    path('user/add/', views.user_add),
 ]
